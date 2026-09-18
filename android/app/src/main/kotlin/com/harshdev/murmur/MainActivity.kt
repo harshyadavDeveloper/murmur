b1a0dc17db3cd5ec89ff7bd6b2006a11308618a0
Harsh Yadav <harshyadav5939@gmail.com>
2026-09-19 00:52:42 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/harshdev/murmur/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.harshdev.murmur
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_local_notifications
+import isar_community_flutter_libs
+import speech_to_text
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
+}
